fad6e7a1d442f280b66b319a87989ce5df8abaac
Esteban <esteban@attitude.cl>
2025-10-15 21:26:12 -0300

    init

---
+++ packages.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+
+brew tap pakerwreah/calendr
+brew install --cask 1password
+brew install --cask affinity-designer
+brew install --cask affinity-photo
+brew install --cask alfred
+brew install alienator88-sentinel
+brew install bash
+brew install bat
+brew install --cask bettermouse
+brew install --cask beyond-compare
+brew install --cask bitwarden
+brew install --cask brave-browser
+brew install --cask bruno
+brew install --cask calendr
+brew install --cask calibre
+brew install --cask claude-code
+brew install codex
+brew install --cask command-tab-plus
+brew install --cask commander-one
+brew install composer
+brew install --cask coteditor
+brew install --cask cryptomator
+brew install cv
+brew install --cask daisydisk
+brew install --cask dbeaver-community
+brew install --cask displaylink
+brew install fastfetch
+brew install --cask ferdium
+brew install ffmpeg
+brew install --cask filebot
+brew install --cask firefox
+brew install --cask font-iosevka-nerd-font
+brew install --cask font-iosevka-term-nerd-font
+brew install gemini-cli
+brew install --cask ghostty
+brew install git
+brew install --cask github
+brew install --cask google-drive
+brew install gulp-cli
+brew install handbrake
+brew install --cask iconchamp
+brew install --cask iina
+brew install --cask imageoptim
+brew install --cask jordanbaird-ice
+brew install --cask kextviewr
+brew install --cask knockknock
+brew install --cask lm-studio
+brew install --cask localsend
+brew install --cask lulu
+brew install markedit
+brew install mas
+brew install --cask macwhisper
+brew install --cask mediainfo
+brew install --cask microsoft-remote-desktop
+brew install --cask middleclick
+brew install mkvtoolnix
+brew install mpv
+brew install node
+brew install --cask nordvpn
+brew install --cask obsidian
+brew install opencode
+brew install openssl
+brew install --cask orbstack
+brew install --cask path-finder
+brew install pearcleaner
+brew install php
+brew install php-cs-fixer
+brew install --cask qspace-pro
+brew install qwen-code
+brew install --cask rectangle-pro
+brew install --cask shottr
+brew install --cask signal
+brew install specify
+brew install --cask stay
+brew install --cask sublime-merge
+brew install --cask sublime-text
+brew install --cask superkey
+brew install tailspin
+brew install tailwindcss
+brew install --cask taskexplorer
+brew install --cask the-unarchiver
+brew install --cask transmission
+brew install --cask tuxera-ntfs
+brew install --cask typora
+brew install --cask uninstallpkg
+brew install --cask utm
+brew install --cask visual-studio-code
+brew install volta
+brew install --cask windsurf
+brew install wp-cli
+brew install yarn
+brew install yt-dlp
+brew install --cask zoom
+brew install aliases
+brew install coreutils
+brew install --cask elmedia-player
+brew install --cask unite
+brew install zsh-autosuggestions
+brew install zsh-completions
+brew install zsh-syntax-highlighting
+brew install python
+brew install sass/sass/sass
+brew install tailscale
+brew install github-copilot-cli
+brew install gh
+brew install prettier
+brew install --cask ungoogled-chromium
+brew install --cask font-inconsolata-nerd-font
+brew install --cask font-arial
+brew install --cask font-cantarell
+brew install --cask font-roboto
+brew install --cask font-iosevka
+brew install --cask font-esteban
+brew install fastmod
+
+npm install -g claude-code-wakatime
+npm install -g postcss
+npm install -g postcss-cli
